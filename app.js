6ecc0597f721aa835336a2b632ee4722349518c0
Kazu <kazuki.ino01@gmail.com>
2025-12-29 21:09:57 +0900

    Mapとオブジェクトでデータを整理

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const fs = require('node:fs');
 const readline = require('node:readline');
 const rs = fs.createReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ input: rs });
+const prefectureDataMap = new Map(); // key: 都道府県 value: 集計データのオブジェクト
 
 rl.on('line', lineString => {
   const columns = lineString.split(',');
@@ -11,8 +12,29 @@ rl.on('line', lineString => {
   const popu = parseInt(columns[2]);
 
   if (year === 2016 || year === 2021) {
-    console.log(year);
-    console.log(prefecture);
-    console.log(popu);
+    let value = null;
+    if (prefectureDataMap.has(prefecture)) {
+      value = prefectureDataMap.get(prefecture);
+    } else {
+      value = {
+        before: 0,
+        after: 0,
+        change: null
+      };
+    }
+    if (year === 2016) {
+      value.before = popu;
+    }
+    if (year === 2021) {
+      value.after = popu;
+    }
+    prefectureDataMap.set(prefecture, value);
   }
+});
+
+rl.on('close', () => {
+  for (const [key, value] of prefectureDataMap) {
+    value.change = value.after / value.before;
+  }
+  console.log(prefectureDataMap);
 });
